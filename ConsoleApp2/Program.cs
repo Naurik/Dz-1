940944ab7fbaed28f0eb76f26dd3cc6c4b55f9b0
Nauryzbai <nauzik@mail.ru>
2019-01-06 21:21:00 +0600

    Домашнее задание1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = 5;
+            Console.Write("numbers = " + numbers);
+            Console.Read();
+        }
+    }
+}
